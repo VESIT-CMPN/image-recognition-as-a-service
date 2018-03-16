1a186f3ba16fbada85b9d66f4c425aa4fae995ac
pavan-08 <pavanchhatpar@gmail.com>
2018-03-16 10:11:29 +0530

    model setup separation

--- server/api.py
+++ server/api.py
@@ -34,10 +34,8 @@ import argparse
 import os.path
 import re
 import sys
-import tarfile
 
 import numpy as np
-from six.moves import urllib
 import tensorflow as tf
 
 from flask import Flask, request
@@ -46,10 +44,6 @@ from collections import OrderedDict
 import base64
 
 FLAGS = None
-
-# pylint: disable=line-too-long
-DATA_URL = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
-# pylint: enable=line-too-long
 
 app = Flask(__name__)
 api = Api(app)
@@ -185,32 +179,8 @@ def run_inference_on_image(image):
         #     print('%s (score = %.5f)' % (human_string, score))
         return predictions
 
-
-def maybe_download_and_extract():
-    """Download and extract model tar file."""
-    dest_directory = FLAGS.model_dir
-    if not os.path.exists(dest_directory):
-        os.makedirs(dest_directory)
-    filename = DATA_URL.split('/')[-1]
-    filepath = os.path.join(dest_directory, filename)
-    if not os.path.exists(filepath):
-        def _progress(count, block_size, total_size):
-            sys.stdout.write('\r>> Downloading %s %.1f%%' % (
-                filename, float(count * block_size) / float(total_size) * 100.0))
-            sys.stdout.flush()
-        filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
-        print()
-        statinfo = os.stat(filepath)
-        print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
-    tarfile.open(filepath, 'r:gz').extractall(dest_directory)
-
-
 def main(_):
-    maybe_download_and_extract()
-    # image = (FLAGS.image_file if FLAGS.image_file else
-    #         os.path.join(FLAGS.model_dir, 'cropped_panda.jpg'))
-    # run_inference_on_image(image)
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
 
 
 if __name__ == '__main__':
@@ -224,7 +194,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--model_dir',
         type=str,
-        default='/tmp/imagenet',
+        default='./imagenet',
         help="""\
         Path to classify_image_graph_def.pb,
         imagenet_synset_to_human_label_map.txt, and
